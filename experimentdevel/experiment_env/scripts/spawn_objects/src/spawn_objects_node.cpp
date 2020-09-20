052d69fc70a78b8fe8329c89ce24b14811c194dd
Jason Dekarske <dekarskej@gmail.com>
2020-09-20 14:22:49 -0700

    We now spawn a set of cubes in an array above the table

--- experimentdevel/experiment_env/scripts/spawn_objects/src/spawn_objects_node.cpp
+++ experimentdevel/experiment_env/scripts/spawn_objects/src/spawn_objects_node.cpp
@@ -7,6 +7,7 @@
 
 const float cube_size = 0.064;
 const float cube_mass = 1.0;
+const float offset_height = 0.05; // don't spawn *in* the table
 
 // A lot of help from https://github.com/JenniferBuehler/gazebo-pkgs/blob/master/gazebo_test_tools/src/cube_spawner.cpp!
 
@@ -105,13 +106,25 @@ bool spawncube(float x, float y, float z, uint id, float red=-1, float green=-1,
 
 bool callback(spawn_objects::spawn_objects::Request &request, spawn_objects::spawn_objects::Response &response)
 {
-    int all_id = 0;
-    bool status = true;
-    for (size_t x = 0; x < 6; x++)
+    // Spawn a clump of objects centered at the middle of the object (table for now)
+    // TODO: generalize this to any model in the scene, now it is hardcoded for the table
+    uint8_t width_objs = request.width; // number of objects along the width (x)
+    uint8_t length_objs = request.length; // number of objects along the length (y)
+
+    float table_center[] = {0.2, -0.6}; // coords for the center of the table (x,y)
+    float table_height = 1.0;
+    float cube_separation = cube_size + 0.1; // defined from the middle of the cube
+
+    float objectset_dim[] = {cube_separation * (width_objs-1), cube_separation * (length_objs-1)}; //distance of the sides of the object set
+    float start_coord[] = {table_center[0]-objectset_dim[0]/2,table_center[1]-objectset_dim[1]/2}; //the coordinates of the first object
+
+    int all_id = 0; // increment the ID. TODO, get the current max cube ID
+    bool status = true; // make sure they ALL spawn successfully
+    for (float x = start_coord[0]; x <= start_coord[0] + objectset_dim[0]; x=x+cube_separation)
     {
-        for (size_t y = 0; y < 11; y++)
+        for (float y = start_coord[1]; y <= start_coord[1] + objectset_dim[1]; y = y+cube_separation)
         {
-            status = spawncube(x/10.0,-0.2-y/10.0,1.05,all_id) && status;
+            status = spawncube(x,y,table_height+offset_height,all_id) && status;
             all_id++;
         }
     }
