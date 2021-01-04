ecae51d83b0fd6f5f89c140892819196a36a6301
Jason Dekarske <dekarskej@gmail.com>
2021-01-03 22:59:13 -0800

    name change for generalization

---
+++ experimentdevel/rosplan_interface_ur/include/RPGoto.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <ros/console.h>
+// #include <vector>
+// #include <iostream>
+// #include <fstream>
+// #include <math.h>
+#include <rosplan_action_interface/RPActionInterface.h>
+
+// Position
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Point.h>
+
+// to actually move
+#include <target_pose/target_pose_node.h>
+
+namespace KCL_rosplan
+{
+
+	class RPGoto : public RPActionInterface
+	{
+
+	private:
+		target_pose_node *tp;
+		std::map<std::string, double> getWaypointCoordinates(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr &msg);
+
+	public:
+		/* constructor */
+		RPGoto(ros::NodeHandle &nh);
+
+		/* listen to and process action_dispatch topic */
+		bool concreteCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr &msg);
+	};
+} // namespace KCL_rosplan
--- experimentdevel/rosplan_interface_ur/src/RPGoto.cpp
+++ experimentdevel/rosplan_interface_ur/src/RPGoto.cpp
@@ -1,33 +1,26 @@
-#include "RPur.h"
+#include <RPGoto.h>
 
 /* The implementation of RPTutorial.h */
 namespace KCL_rosplan
 {
-
-	/* constructor */
-	RPur::RPur(ros::NodeHandle &nh)
+	RPGoto::RPGoto(ros::NodeHandle &nh)
 	{
-		// perform setup
-		manipulator_group = new moveit::planning_interface::MoveGroupInterface("manipulator");
-		gripper_group = new moveit::planning_interface::MoveGroupInterface("gripper");
-
-		// 	// move to neutral
-		manipulator_group->setNamedTarget("vertical");
-		manipulator_group->move();
+		tp = new target_pose_node();
+		tp->initPosition();
 	}
 
 	/* action dispatch callback */
-	bool RPur::concreteCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr &msg)
+	bool RPGoto::concreteCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr &msg)
 	{
 		// complete the action (test)
 		ROS_INFO("KCL: (%s) TUTORIAL Action starting.", msg->name.c_str());
 
-		std::map<std::string, double> coords = getWaypointCoordinates(msg);
+		// std::map<std::string, double> coords = getWaypointCoordinates(msg);
 
-		return move(coords["x"], coords["y"], coords["z"]);
+		return tp->move(coords["x"], coords["y"], coords["z"]);
 	}
 
-	std::map<std::string, double> RPur::getWaypointCoordinates(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr &msg)
+	std::map<std::string, double> RPGoto::getWaypointCoordinates(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr &msg)
 	{
 		// msg->parameters is probably:
 		// v-arm
@@ -63,22 +56,6 @@ namespace KCL_rosplan
 
 		return coords;
 	}
-
-	bool RPur::move(float x_des, float y_des, float z_des)
-	{
-		geometry_msgs::Pose target_pose;
-		target_pose.orientation.x = 0.707;
-		target_pose.orientation.y = 0.0;
-		target_pose.orientation.z = -.707;
-		target_pose.orientation.w = 0.0;
-		target_pose.position.x = x_des;
-		target_pose.position.y = y_des;
-		target_pose.position.z = z_des;
-		manipulator_group->setPoseTarget(target_pose, "flange");
-
-		bool success = (manipulator_group->move() == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-		return success;
-	}
 } // namespace KCL_rosplan
 
 /*-------------*/
@@ -88,7 +65,7 @@ namespace KCL_rosplan
 int main(int argc, char **argv)
 {
 
-	ros::init(argc, argv, "rosplan_tutorial_action", ros::init_options::AnonymousName);
+	ros::init(argc, argv, "rosplan_ur_goto_action", ros::init_options::AnonymousName);
 	ros::NodeHandle nh("~");
 
 	// definitely necessary...TODO
@@ -96,8 +73,8 @@ int main(int argc, char **argv)
 	spinner.start();
 
 	// create PDDL action subscriber
-	KCL_rosplan::RPur rpti(nh);
-	rpti.runActionInterface();
+	KCL_rosplan::RPGoto rpg(nh);
+	rpg.runActionInterface();
 
 	ros::waitForShutdown();
 	return 0;
