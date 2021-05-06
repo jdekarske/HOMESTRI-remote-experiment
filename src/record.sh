479334ccde501d4cb137824f8793cb09fe25963a
Jason Dekarske <dekarskej@gmail.com>
2021-05-06 00:10:06 +0000

    720p works uncompressed?

--- src/record.sh
+++ src/record.sh
@@ -1,1 +1,2 @@
-rosrun image_view video_recorder image:=/camera/remote_camera_rack/image_raw _fps:=36
+rosrun image_view video_recorder image:=/camera/remote_camera_rack/image_raw _fps:=30
+#  _image_transport:=compressed
